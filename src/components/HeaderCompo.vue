3b44701b5efc9209ceae271d4f4e9c8173a173b8
leiladasilva <leilasilvasouza349@gmail.com>
2024-09-30 10:45:43 -0300

    ok #10

--- src/components/HeaderCompo.vue
+++ src/components/HeaderCompo.vue
@@ -1,3 +1,7 @@
+
+
+
+
 <script setup>
   import { defineProps } from 'vue';
 
@@ -7,16 +11,14 @@
       required: true,
     },
   });
-
 </script>
 
-  
-   
-  
 <template>
-    <div class="txt d-flex flex-row justify-content-center bg-secondary flex-wrap">
-        <h3><RouterLink to="/paginainicial"><span style='font-size:60px;'>&#128214;</span></RouterLink> {{props.title}}</h3>
-    </div>
+  <header class="header-container">
+    <img src="../assets/menu1.png" alt="Imagem 1" class="header-image" />
+    <h3><RouterLink to="/paginainicial"><span style='font-size:60px;'>&#128214;</span></RouterLink> {{props.title}}</h3>
+   <li><RouterLink to="/cadastro"><img src="../assets/menu.png"  alt="Imagem 2" class="header-image"></RouterLink></li>
+  </header>
     <slot> <nav>
         <ul class=" d-flex bg-dark text-white flex-row justify-content-around">
             <li><RouterLink to="/cadastro">Cadastro</RouterLink></li>
@@ -28,10 +30,28 @@
            <li><RouterLink to="/suspense">Suspense</RouterLink></li>
           </ul>
     </nav></slot>
-     
-   
- </template>
- <style scoped>
+</template>
+
+<style scoped>
+.header-container {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #6c757d;
+  padding: 20px;
+  border-radius: 1px;
+}
+
+.header-title {
+  font-size: 2rem;
+  color: #fff;
+}
+
+.header-image {
+  width: 100px;
+  height: auto;
+  border-radius: 50%;
+}
 .p-2{
     padding: 0px;
 }
@@ -53,4 +73,4 @@
      height: 100px;
      width: 100px;
  }
- </style>
+</style>
